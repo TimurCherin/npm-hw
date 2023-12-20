aa08ab868f63ec3398a3ab5b87740d2f59e2124b
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-12-21 00:32:44 +0200

    add

--- project/index.js
+++ project/index.js
@@ -1,6 +1,23 @@
 const btn = document.getElementById("btn")
 const text = document.querySelector(".text")
+const form = document.querySelector(".form")
+const userName = document.querySelector(".user-name")
+const userPhone = document.querySelector(".user-phone")
+const userEmail = document.querySelector(".user-email")
+
 btn.addEventListener("click", onClick)
 function onClick() {
     text.classList.add("move")
+}
+
+form.addEventListener("submit", onSubmit)
+function onSubmit(e) {
+    e.preventDefault()
+    const data = e.currentTarget.elements;
+    const name = data.name.value
+    const phone = data.phone.value
+    const email = data.email.value
+    userName.textContent = name
+    userPhone.textContent = phone
+    userEmail.textContent = email 
 }
